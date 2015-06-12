1f7fa396c81b7a0f0bba7ccd2410cf9cebd3b359
tsara27 <tsara.tf@gmail.com>
2015-06-13 05:44:30 +0700

    add response

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,23 @@
-function start() {
+var exec = require("child_process").exec;
+
+function start(response) {
   console.log("Request handler 'start' was called.");
+  var content = "empty";
 
-  function sleep(milliSeconds) {
-    var startTime = new Date().getTime();
-    while (new Date().getTime() < startTime + milliSeconds);
-  }
+  exec("ls -lah", function (error, stdout, stderr) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write(stdout);
+    response.end();
+  });
 
-  sleep(10000);
-
-  return "Hello Start";
+  return content;
 }
 
-function upload() {
+function upload(response) {
   console.log("Request handler 'upload' was called");
-  return "Hello Upload";
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.write("Hello Upload");
+  response.end();
 }
 
 exports.start = start;
